49992fe4ff304093468a513fb3061f0e0c579d92
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-09 11:03:05 +0200

    working character listing

--- lotr-api/src/components/Characters.js
+++ lotr-api/src/components/Characters.js
@@ -5,15 +5,23 @@ function Characters() {
     const [characters, setCharacters] = useState([])
 
     useEffect(() => {
-        axios.post('https://the-one-api.herokuapp.com/v1/character',{
-            headers:{
-                Authorization: 'pmF8BDZT97okBAtf7_Ui'
+        axios.get('https://the-one-api.herokuapp.com/v1/character', {
+            headers: {
+                Authorization: 'Bearer pmF8BDZT97okBAtf7_Ui'
             }
         })
-            .then(response => console.log(response))
+            .then(response => setCharacters(response.data.docs))
     })
-    return(
-        <div>aaaaaaaaa</div>
+    console.log(characters)
+    const iter = characters.map((item) => (
+            <li key={item.id}>{item.name}</li>
+        )
+    )
+
+    return (
+        <div>
+            {iter}
+        </div>
     )
 }
 
