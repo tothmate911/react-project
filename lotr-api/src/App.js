b5248b3b15e6a48c14ef18232c1da09a01a7ff89
noemi <gynoemi33@gmail.com>
2020-06-09 14:30:04 +0200

    Style the large elements

--- lotr-api/src/App.js
+++ lotr-api/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./css/App.css";
-import Header from "./components//layout/Header";
+import Header from "./components/layout/Header";
+import styled from "styled-components";
+
+const Main = styled.div`
+  margin-top: 20px;
+  margin-left: 20%;
+  margin-right: 20%;
+  text-align: left;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <Main></Main>
     </div>
   );
 }
