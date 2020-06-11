478ede44cf699d2bfcc1ebbd69be27c3c59cc11e
noemi <gynoemi33@gmail.com>
2020-06-11 21:47:55 +0200

    Fix all the bugs

--- lotr-api/src/App.js
+++ lotr-api/src/App.js
@@ -1,46 +1,58 @@
-import React, {useContext} from 'react';
-import './css/App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Characters from './components/pages/Characters';
-import Header from './components/layout/Header';
-import MainPage from './components/pages/MainPage';
-import Movie from './components/pages/Movie';
-import styled from 'styled-components';
-import Book from './components/pages/Book';
+import React, { useContext } from "react";
+import "./css/App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Characters from "./components/pages/Characters";
+import Header from "./components/layout/Header";
+import MainPage from "./components/pages/MainPage";
+import Movie from "./components/pages/Movie";
+import styled from "styled-components";
+import Book from "./components/pages/Book";
 import Character from "./components/pages/Character";
-import {ThemeContext} from './context/ThemeContext';
-import AppTheme from './components/layout/Colors';
+import { ThemeContext } from "./context/ThemeContext";
+import AppTheme from "./components/layout/Colors";
 
 const App = (props) => {
-    const [theme] = useContext(ThemeContext);
-    const currentTheme = AppTheme[theme];
+  const [theme] = useContext(ThemeContext);
+  const currentTheme = AppTheme[theme];
 
-    const containerStyle = {
-        backgroundColor: `${currentTheme.backgroundColor2}`,
-    };
-    const Main = styled.div`
+  const ContainerStyle = styled.div`
+    background-color: ${currentTheme.backgroundColor2};
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    top: 0;
+    left: 0;
+    right: 0;
+    min-height: 100%;
+  `;
+
+  const Main = styled.div`
     margin-top: 20px;
     margin-left: 20%;
     margin-right: 20%;
+    margin-bottom: 20px;
     text-align: left;
     background-color: ${currentTheme.backgroundColor};
     color: ${currentTheme.textColor};
     padding: 40px;
   `;
-    return (
-        <div>
-            <Router>
-                <Header/>
-                <Route exact path="/" component={MainPage}/>
-                <Main>
-                    <Route exact path="/characters" component={Characters}/>
-                    <Route path="/character/:id" component={Character}/>
-                    <Route path="/movie/:id" component={Movie}/>
-                    <Route path="/book/:id" component={Book}/>
-                </Main>
-            </Router>
-        </div>
-    );
-}
+
+  return (
+    <div>
+      <Router>
+        <ContainerStyle>
+          <Header />
+          <Route exact path="/" component={MainPage} />
+          <Main>
+            <Route exact path="/characters" component={Characters} />
+            <Route path="/character/:id" component={Character} />
+            <Route path="/movie/:id" component={Movie} />
+            <Route path="/book/:id" component={Book} />
+          </Main>
+        </ContainerStyle>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- lotr-api/src/components/layout/ThemeToggler.js
+++ lotr-api/src/components/layout/ThemeToggler.js
@@ -1,21 +1,25 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../../context/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import { DataContext } from "../../context/DataContext";
 
 const themeTogglerStyle = {
-  padding: '30px',
-  cursor: 'pointer',
+  padding: "30px",
+  cursor: "pointer",
 };
 
 const ThemeToggler = () => {
   const [theme, setTheme] = useContext(ThemeContext);
+  const dataContext = useContext(DataContext);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    dataContext.setIsBookLoaded(false);
+    dataContext.setIsMovieLoaded(false);
   };
 
   return (
     <div style={themeTogglerStyle} onClick={toggleTheme}>
-      <span title="switch theme">{theme === 'light' ? '🌙' : '☀️'}</span>
+      <span title="switch theme">{theme === "light" ? "🌙" : "☀️"}</span>
     </div>
   );
 };
