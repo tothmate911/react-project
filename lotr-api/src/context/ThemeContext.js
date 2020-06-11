30695ffbc8c6427c1de5e0d8467697c6aa5171cc
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-11 20:49:43 +0200

    default theme changed to 'light'

--- lotr-api/src/context/ThemeContext.js
+++ lotr-api/src/context/ThemeContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = (props) => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
 
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
